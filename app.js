02b6dd1340fbffd2e60abc9629908f233f29e60b
Naz Islam <naz@nazislam.com>
2018-02-26 02:15:20 -0500

    Added form for sending request for dropping recyclables off

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+'use strict';
+
 var express = require('express');               // Importing express
+var bodyParser = require('body-parser');
 var app = express();                            // An instance of express app
 var router = express.Router();                  // An instance of express router to work with routes
+var dropoffRouter = require('./dropoff/crud');
 
-
+dropoffRouter.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', router);                           // Apply the routes to our application
 app.use(express.static('./public'));            // Providing 'public' directory as static
 app.set('views', './public/views');             // Sets the location to find templates for routes
 app.set('view engine', 'pug');                  // Sets Pug as template engine for the app
 
-const PORT = 3000;                              // App will listen to this port
+app.use('/dropoff', dropoffRouter);
+
+const PORT = 5000;                              // App will listen to this port
 
 
 router.get('/', (req, res) => {                 // GET method on path '/'
@@ -28,12 +34,11 @@ router.get('/mapui', (req, res) => {           // Get method on path '/mapui'
 router.get('/mymap', (req, res) => {           // Get method on path '/mymap'
     res.render('mymap');                       // Rendering mapui.pug as template for path 'mymap'
 })
-router.get('/dropoff', (req, res) => {           // Get method on ath '/dropoff'
-    res.render('dropoff');                       // Rendering mapui.pug as template for path 'dropoff'
-})
+
+
 
 
 /***** Listening to port 5000  **/
-app.listen(PORT, (err) => {                             // Listening to the specified port
-    console.log('Running server on port: ' + PORT);
-})
+app.listen(PORT, (req, res) => {
+    console.log('Running ON PORT 3000');
+});
