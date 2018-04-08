50057d98555a107c5e169aa2715fbb640c0a7e7a
Naz Islam <naz@nazislam.com>
2018-04-07 23:08:42 -0400

    '/pickup' route populates pickup requests

--- pickup/model-datastore.js
+++ pickup/model-datastore.js
@@ -8,8 +8,47 @@ const ds = Datastore({
 
 const kind = "Dropoff";
 
+function fromDatastore (obj) {
+  obj.id = obj[Datastore.KEY].id;
+  return obj;
+}
 
+function toDatastore (obj, nonIndexed) {
+  nonIndexed = nonIndexed || [];
+  const results = [];
+  Object.keys(obj).forEach((k) => {
+    if (obj[k] === undefined) {
+      return;
+    }
+    results.push({
+      name: k,
+      value: obj[k],
+      excludeFromIndexes: nonIndexed.indexOf(k) !== -1
+    });
+  });
+  return results;
+}
+
+function list (limit, token, cb) {
+  const q = ds.createQuery([kind])
+    .limit(limit)
+    //.order('title')
+    .start(token);
+
+  ds.runQuery(q, (err, entities, nextQuery) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+    const hasMore = nextQuery.moreResults !== Datastore.NO_MORE_RESULTS ? nextQuery.endCursor : false;
+    cb(null, entities.map(fromDatastore), hasMore);
+  });
+}
+
+
+/*
 function listRequest() {
+  var results = [];
   const query = ds.createQuery(kind);
   ds.runQuery(query)
     .then(results => {
@@ -18,9 +57,12 @@ function listRequest() {
       console.log('Entries');
       entries.forEach(entry => {
         const entryKey = entry[ds.KEY];
-        console.log(entryKey.id, entry);
+        // console.log(entryKey.id, entry);
+        results.push(entry);
+        console.log(entry);
       });
     });
 }
+*/
 
-module.exports = { listRequest };
+module.exports = { list };
--- pickup/pickup.js
+++ pickup/pickup.js
@@ -7,10 +7,28 @@ function getModel() {
     return require('./model-datastore');
 }
 
+pickupRouter.get('/', (req, res, next) => {
+  getModel().list(10, req.query.pageToken, (err, entities, cursor) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.render('pickup.pug', {
+      entries: entities
+      // nextPageToken: cursor
+    });
+  });
+});
+
+/*
 pickupRouter.route('/')
   .get(function(req ,res) {
-    getModel().listRequest();
-    res.render('pickup');
+    var entries = getModel().listRequest();
+    console.log('----')
+    console.log(entries);
+    console.log('----')
+    res.render('pickup', { entries: entries, a: [1,2,3] });
   });
+*/
 
 module.exports = pickupRouter;
